19a3d3f5c495bb6c9f53e2f96e5d08fdd8dfd2eb
Gabriel Matute <orangedev8@gmail.com>
2025-01-15 18:44:52 -0600

    working with different methods

--- app.py
+++ app.py
@@ -1,2 +1,31 @@
-name = input("What is your name ")
-print("Hello " + name)
+# Variables
+x =  1
+x = x + 2
+print(x)
+
+# strings
+x = "abc"
+y = "de"
+z = x + y
+z
+
+print(z)
+
+z = z[1:3]
+print(z)
+
+# functions
+def f(x):
+    y = x+2
+    return y
+print(f(5))
+
+# if statements
+x = 2
+if x+1 == 3:
+    print(x)
+
+# loops
+for i in range(1,100):
+    if i%2 == 0:
+        print(i)
